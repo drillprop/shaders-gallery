eea8c7024eb8613492dd6991965200eba766e41c
drillprop <drylbartosz@gmail.com>
2025-01-06 22:56:03 +0100

    add uniform to third

--- src/components/shaders/Third/Third.tsx
+++ src/components/shaders/Third/Third.tsx
@@ -1,16 +1,28 @@
+"use client";
+
 import vertexShader from "./thirdVertex.vert";
 import fragmentShader from "./thirdFragment.frag";
 import { Scene } from "../../Scene/Scene";
+import { useMemo } from "react";
+import { Vector2 } from "three";
 
 export const Third = () => {
+  const uniforms = useMemo(
+    () => ({
+      uFrequency: { value: new Vector2(10, 20) },
+    }),
+    []
+  );
+
   return (
     <Scene>
       <mesh>
-        <planeGeometry args={[10, 10, 32, 32]} />
+        <planeGeometry args={[1, 1, 32, 32]} />
         <rawShaderMaterial
           transparent
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
+          uniforms={uniforms}
         />
       </mesh>
     </Scene>
--- src/components/shaders/Third/thirdVertex.vert
+++ src/components/shaders/Third/thirdVertex.vert
@@ -5,10 +5,13 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute vec2 uv;
 
+uniform vec2 uFrequency;
+
 void main() {
     
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 2.0) * 0.5;
+    modelPosition.z += sin(modelPosition.x * uFrequency.x) * 0.1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
