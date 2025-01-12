5c36a2079153e45e0fe3e5bdd56e16e531155cfe
drillprop <drylbartosz@gmail.com>
2025-01-12 22:02:31 +0100

    add useGui to third shader page

--- src/app/shaders/third/page.tsx
+++ src/app/shaders/third/page.tsx
@@ -1,88 +1,30 @@
 "use client";
 
-import { Third } from "@/components/shaders/Third/Third";
-import { useRef, useEffect, useState } from "react";
-import GUI from "lil-gui";
 import { Scene } from "@/components/Scene/Scene";
-import { useDebugParam } from "@/hooks/useDebugParam";
+import { Third } from "@/components/shaders/Third/Third";
+import { useGui } from "@/hooks/useGui";
 
 export default function ThirdShaderPage() {
-  const guiRef = useRef<GUI | null>(null);
-  const [frequency, setFrequency] = useState({
-    x: 40,
-    y: 20,
-  });
-  const [rgb, setRgb] = useState({
-    r: 0.6,
-    g: 0.3,
-    b: 0.1,
-  });
-  const isDebugOn = useDebugParam();
-
-  useEffect(() => {
-    if (!isDebugOn) {
-      return;
+  const controls = useGui(
+    { r: 0.6, g: 0.3, b: 0.1, x: 2, y: 5 },
+    {
+      r: { min: 0, max: 1, step: 0.01 },
+      g: { min: 0, max: 1, step: 0.01 },
+      b: { min: 0, max: 1, step: 0.01 },
+      x: { min: 0, max: 50, step: 0.01 },
+      y: { min: 0, max: 50, step: 0.01 },
     }
-    guiRef.current = new GUI();
-
-    const gui = guiRef.current;
-
-    gui
-      .add(rgb, "r")
-      .min(0)
-      .max(1)
-      .step(0.01)
-      .onChange((value: number) => {
-        setRgb((prev) => ({ ...prev, r: value }));
-      });
-
-    gui
-      .add(rgb, "g")
-      .min(0)
-      .max(1)
-      .step(0.01)
-      .onChange((value: number) => {
-        setRgb((prev) => ({ ...prev, g: value }));
-      });
-
-    gui
-      .add(rgb, "b")
-      .min(0)
-      .max(1)
-      .step(0.01)
-      .onChange((value: number) => {
-        setRgb((prev) => ({ ...prev, b: value }));
-      });
-
-    gui
-      .add(frequency, "x")
-      .min(0)
-      .max(50)
-      .step(0.01)
-      .onChange((value: number) => {
-        setFrequency((prev) => ({ ...prev, x: value }));
-      });
-
-    gui
-      .add(frequency, "y")
-      .min(0)
-      .max(50)
-      .step(0.01)
-      .onChange((value: number) => {
-        setFrequency((prev) => ({ ...prev, y: value }));
-      });
-
-    return () => {
-      if (guiRef.current) {
-        guiRef.current.destroy();
-        guiRef.current = null;
-      }
-    };
-  }, []);
+  );
 
   return (
     <Scene>
-      <Third x={frequency.x} y={frequency.y} r={rgb.r} g={rgb.g} b={rgb.b} />
+      <Third
+        x={controls.x}
+        y={controls.y}
+        r={controls.r}
+        g={controls.g}
+        b={controls.b}
+      />
     </Scene>
   );
 }
